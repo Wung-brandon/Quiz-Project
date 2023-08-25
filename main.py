65c579f2f57fc82b92f21da7dbb479df351cde14
wung brandon <wungbrandon27@gmail.com>
2023-08-25 07:27:43 +0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 
@@ -11,10 +12,11 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
 
 
-while quiz.still_has_questions():
-    quiz.next_question()
+#while quiz.still_has_questions():
+ #   quiz.next_question()
     
 print("You have completed the quiz")
 print(f"Your final score is {quiz.score}")
